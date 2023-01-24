1e3ee3ec27d7194b75b7170f12636de409f65f9f
cbroschinski <broschinski@uni-bielefeld.de>
2023-01-24 10:35:09 +0100

    added cmd args to use variable schema/test file input

--- validate.py
+++ validate.py
@@ -1,8 +1,15 @@
+import argparse
+
 from lxml import etree
 
-xmlschema_doc = etree.parse("opencost.xsd")
+parser = argparse.ArgumentParser()
+parser.add_argument("schema_file", help="The XML schema file to use for validation")
+parser.add_argument("test_file", help="The XML file to validate")
+args = parser.parse_args()
+
+xmlschema_doc = etree.parse(args.schema_file)
 xmlschema = etree.XMLSchema(xmlschema_doc)
 
-example = etree.parse("closed_access.xml")
+example = etree.parse(args.test_file)
 
 xmlschema.assertValid(example)
